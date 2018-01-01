f07ed1135aece42edf2bdefd1016b190ddec90c4
Carlos José Bulado <carlosbulado@gmail.com>
2017-12-31 19:06:51 -0500

    Initial Commit

---
+++ FSP_Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FSP_Notes
+//
+//  Created by Carlos José Bulado on 2017-12-31.
+//  Copyright © 2017 Carlos José Bulado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FSP_NotesTests/FSP_NotesTests.swift
@@ -0,0 +1,34 @@
+//
+//  FSP_NotesTests.swift
+//  FSP_NotesTests
+//
+//  Created by Carlos José Bulado on 2017-12-31.
+//  Copyright © 2017 Carlos José Bulado. All rights reserved.
+//
+
+import XCTest
+@testable import FSP_Notes
+
+class FSP_NotesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
